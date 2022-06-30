632dfde8940171104455135aecbb8c36f9e3919c
cesarbouzas <cesarbouzaspaw@gmail.com>
2022-06-30 12:29:59 +0200

    Update Principal.java

--- 002_IDEs/001_Eclipse/220610_Ejercio_race_Control/Race_control/src/principal/Principal.java
+++ 002_IDEs/001_Eclipse/220610_Ejercio_race_Control/Race_control/src/principal/Principal.java
@@ -1,9 +1,15 @@
 package principal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import clases_Objeto.*;
 public class Principal {
 
 	public static void main(String[] args) {
+		System.out.println(LocalDate.now().format(DateTimeFormat)
+		
 		Pantalla.borrar();
+		
 		Menu.principal(); 
 }
 }
